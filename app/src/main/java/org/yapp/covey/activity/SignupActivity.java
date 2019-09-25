74b6aad602227cbb0bfd63e1acad0407d3399714
kin16742 <kin16742@naver.com>
2019-09-25 18:39:22 +0900

    signup_login
    fragment로 구현 간단히 layout만

--- app/src/main/java/org/yapp/covey/activity/SignupActivity.java
+++ app/src/main/java/org/yapp/covey/activity/SignupActivity.java
@@ -1,10 +1,13 @@
 package org.yapp.covey.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import org.yapp.covey.R;
+import org.yapp.covey.fragment.*;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -12,5 +15,16 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Signup_LoginFragment loginFragment = new Signup_LoginFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // 여기에서 프래그먼트 트랜잭션, 백스택, 애니메이션 등을 설정합니다.
+        // -----------------------------------------------------------------
+        fragmentTransaction.add(R.id.fragment_container_signup, loginFragment);
+        // -----------------------------------------------------------------
+        fragmentTransaction.commit();
     }
 }

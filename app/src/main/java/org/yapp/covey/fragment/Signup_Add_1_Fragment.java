68ec037a2109ebf06c12cbabcc944ea755f51158
kin16742 <kin16742@naver.com>
2019-11-02 15:50:16 +0900

    변경 레이아웃

--- app/src/main/java/org/yapp/covey/fragment/Signup_Add_1_Fragment.java
+++ app/src/main/java/org/yapp/covey/fragment/Signup_Add_1_Fragment.java
@@ -1,11 +1,15 @@
 package org.yapp.covey.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.yapp.covey.R;
 import org.yapp.covey.activity.SignupActivity;
@@ -23,6 +27,53 @@ public class Signup_Add_1_Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_signup_add_1, container, false);
         RelativeLayout next_button = view.findViewById(R.id.signup_add_1_button);
         ImageView prev_button = view.findViewById(R.id.signup_add_1_arrow);
+
+        Spinner genderSpinner = view.findViewById(R.id.signup_add_1_spinner_gender);
+        ArrayAdapter genderAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.gender)){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                    return false;
+                else
+                    return true;
+            }
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0)
+                    tv.setTextColor(Color.GRAY);
+                else
+                    tv.setTextColor(Color.BLACK);
+                return view;
+            }
+        };
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
+
+        Spinner ageSpinner = view.findViewById(R.id.signup_add_1_spinner_age);
+        ArrayAdapter ageAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.age)){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0)
+                    return false;
+                else
+                    return true;
+            }
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0)
+                    tv.setTextColor(Color.GRAY);
+                else
+                    tv.setTextColor(Color.BLACK);
+                return view;
+            }
+        };
+        ageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+
 
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override

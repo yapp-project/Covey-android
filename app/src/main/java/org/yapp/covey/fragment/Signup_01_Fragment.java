a5f5fc43c6f3afb0f961a25d3771bae78a983e85
kin16742 <kin16742@naver.com>
2019-11-02 19:48:07 +0900

    signup fragment 작성중
    남은거 : spinner, kakao/facebook login, data 전송

--- app/src/main/java/org/yapp/covey/fragment/Signup_01_Fragment.java
+++ app/src/main/java/org/yapp/covey/fragment/Signup_01_Fragment.java
@@ -1,9 +1,12 @@
 package org.yapp.covey.fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -21,15 +24,37 @@ public class Signup_01_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup_01, container, false);
-        RelativeLayout next_button = view.findViewById(R.id.signup_01_button);
+        final RelativeLayout next_button = view.findViewById(R.id.signup_01_button);
         ImageView prev_button = view.findViewById(R.id.signup_01_arrow);
-
-        next_button.setOnClickListener(new View.OnClickListener() {
+        final EditText phoneNum = view.findViewById(R.id.signup_01_phoneNum);
+        phoneNum.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                ((SignupActivity)getActivity()).replaceFragment(Signup_02_Fragment.newInstance());
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(phoneNum.getText().length() == 11) {
+                    next_button.setBackground(getResources().getDrawable(R.drawable.red_square));
+                    next_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Fragment next = Signup_02_Fragment.newInstance();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("phoneNum", phoneNum.getText().toString());
+                            next.setArguments(bundle);
+                            ((SignupActivity)getActivity()).replaceFragment(next);
+                        }
+                    });
+                }
+                else {
+                    next_button.setBackground(getResources().getDrawable(R.drawable.gray_square));
+                    next_button.setOnClickListener(null);
+                }
             }
+            @Override
+            public void afterTextChanged(Editable arg0) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
         });
+
+
         prev_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/org/yapp/covey/fragment/Signup_02_Fragment.java
+++ app/src/main/java/org/yapp/covey/fragment/Signup_02_Fragment.java
@@ -1,11 +1,15 @@
 package org.yapp.covey.fragment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import org.yapp.covey.R;
 import org.yapp.covey.activity.SignupActivity;
@@ -21,15 +25,42 @@ public class Signup_02_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signup_02, container, false);
-        RelativeLayout next_button = view.findViewById(R.id.signup_02_button);
-        ImageView prev_button = view.findViewById(R.id.signup_02_arrow);
+        String phoneNumStr = "";
+        if(getArguments() != null) phoneNumStr = getArguments().getString("phoneNum");
 
-        next_button.setOnClickListener(new View.OnClickListener() {
+        final RelativeLayout next_button = view.findViewById(R.id.signup_02_button);
+        ImageView prev_button = view.findViewById(R.id.signup_02_arrow);
+        final TextView phoneNum = view.findViewById(R.id.signup_02_phoneNum);
+        final EditText certification = view.findViewById(R.id.signup_02_certification);
+
+        phoneNum.setText(phoneNumStr);
+        certification.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                ((SignupActivity)getActivity()).replaceFragment(Signup_03_Fragment.newInstance());
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(certification.getText().length() == 7) {
+                    next_button.setBackground(getResources().getDrawable(R.drawable.red_square));
+                    next_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Fragment next = Signup_03_Fragment.newInstance();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("phoneNum", phoneNum.getText().toString());
+                            next.setArguments(bundle);
+                            ((SignupActivity)getActivity()).replaceFragment(next);
+                        }
+                    });
+                }
+                else {
+                    next_button.setBackground(getResources().getDrawable(R.drawable.gray_square));
+                    next_button.setOnClickListener(null);
+                }
             }
+            @Override
+            public void afterTextChanged(Editable arg0) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
         });
+
         prev_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

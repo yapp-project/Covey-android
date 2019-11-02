06d94711526f3c69e80af3cc29d3033342d06e24
GowoonJ <jgw971229@naver.com>
2019-11-02 16:53:48 +0900

    add location post recycler view

---
+++ app/src/main/java/org/yapp/covey/adapter/AdapterLocationList.java
@@ -0,0 +1,67 @@
+package org.yapp.covey.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.yapp.covey.R;
+import org.yapp.covey.etc.ItemPostVO;
+
+import java.util.ArrayList;
+
+public class AdapterLocationList extends RecyclerView.Adapter<AdapterLocationList.ViewHolder> {
+    ArrayList<ItemPostVO> mDataList;
+
+    public AdapterLocationList(ArrayList<ItemPostVO> Data) {
+        this.mDataList = Data;
+    }
+
+    @NonNull
+    @Override
+    public AdapterLocationList.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_recycler_home_location,parent,false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.width = (int) (parent.getWidth()*0.411);
+        layoutParams.height =(int) (parent.getHeight()*0.271);
+        view.setLayoutParams(layoutParams);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterLocationList.ViewHolder holder, int position) {
+        ItemPostVO data = mDataList.get(position);
+        String time = data.getStartDate()+data.getEndDate();
+        String location = data.getAddress1()+data.getAddress2()+data.getAddress3();
+
+        holder.tvTime.setText(time);
+        holder.tvLocation.setText(location);
+        holder.tvMoney.setText(data.getPay());
+        holder.tvName.setText(data.getTitle());
+        holder.tvCategory.setText(data.getCategory());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvCategory, tvName, tvMoney, tvLocation, tvTime;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCategory = itemView.findViewById(R.id.tv_category);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvLocation = itemView.findViewById(R.id.tv_location);
+            tvMoney = itemView.findViewById(R.id.tv_money);
+            tvTime = itemView.findViewById(R.id.tv_time);
+        }
+    }
+
+}
--- app/src/main/java/org/yapp/covey/fragment/HomeFragment.java
+++ app/src/main/java/org/yapp/covey/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.StyleSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,18 +18,31 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.yapp.covey.R;
 import org.yapp.covey.adapter.AdapterCategoryList;
+import org.yapp.covey.adapter.AdapterLocationList;
 import org.yapp.covey.etc.CategoryData;
 import org.yapp.covey.etc.ItemDecorationCategory;
+import org.yapp.covey.etc.ItemPostVO;
+import org.yapp.covey.util.Singleton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeFragment extends Fragment {
     ImageButton buttonAlert, buttonFilter;
     TextView tvCome, tvTitle;
-    private RecyclerView listViewCategory;
+    private RecyclerView listViewCategory, recyclerViewLocation, recyclerViewPay;
     private AdapterCategoryList adapterCategory;
+    private AdapterLocationList adapterLocationPost;
     private ArrayList<String> mArrayData = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
+
+    private static final String TAG = "HOME";
+    private static final int DATA_PAY = 1;
+    private static final int DATA_LOCATION = 2;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -37,20 +51,19 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        mArrayData.addAll(Arrays.asList(CategoryData.sData));
+
+        linearLayoutManager = new LinearLayoutManager(getContext()
+                ,LinearLayoutManager.HORIZONTAL,false);
 
         setInitView(rootView);
         setTitle("커비에서 빠르고 편리한\n" + "알바 대타를 경험해보세요!");
+        setRecyclerView();
 
-        mArrayData.addAll(Arrays.asList(CategoryData.sData));
-
-        adapterCategory = new AdapterCategoryList(getContext(),mArrayData);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext()
-                ,LinearLayoutManager.HORIZONTAL,false);
-        listViewCategory.setLayoutManager(linearLayoutManager);
-        listViewCategory.addItemDecoration(new ItemDecorationCategory(getContext(),8f));
-        listViewCategory.setAdapter(adapterCategory);
+        getPostData();
+        recyclerViewLocation.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+        recyclerViewLocation.setAdapter(adapterLocationPost);
 
         return rootView;
     }
@@ -61,6 +74,8 @@ public class HomeFragment extends Fragment {
         tvTitle = view.findViewById(R.id.tv_title);
 
         listViewCategory = view.findViewById(R.id.recycler_category);
+        recyclerViewLocation = view.findViewById(R.id.recycler_distance);
+        recyclerViewPay = view.findViewById(R.id.recycler_money);
     }
 
     private void setTitle(String title){
@@ -68,5 +83,33 @@ public class HomeFragment extends Fragment {
         builder.setSpan(new StyleSpan(Typeface.BOLD),0,2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         builder.setSpan(new StyleSpan(Typeface.BOLD),5,18, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         tvTitle.append(builder);
+    }
+
+    private void setRecyclerView(){
+        adapterCategory = new AdapterCategoryList(getContext(),mArrayData);
+        listViewCategory.setLayoutManager(linearLayoutManager);
+        listViewCategory.addItemDecoration(new ItemDecorationCategory(getContext(),8f));
+        listViewCategory.setAdapter(adapterCategory);
+    }
+
+    public void getPostData(){
+        Singleton.retrofit.addressList(1).enqueue(new Callback<ArrayList<ItemPostVO>>() {
+            @Override
+            public void onResponse(Call<ArrayList<ItemPostVO>> call, Response<ArrayList<ItemPostVO>> response) {
+                if (response.isSuccessful()){
+                    if (response.code()==200) {
+                        adapterLocationPost = new AdapterLocationList(response.body());
+                        Log.d(TAG, String.valueOf(response.body().get(0)));
+                    }
+                    else
+                        Log.d(TAG, String.valueOf(response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<ItemPostVO>> call, Throwable t) {
+
+            }
+        });
     }
 }

51ed302649e2db1124da627af4d3afc88c82e0b4
GowoonJ <jgw971229@naver.com>
2019-11-08 02:40:39 +0900

    add filter bottom sheet fragment

---
+++ app/src/main/java/org/yapp/covey/fragment/FilterDialogFragment.java
@@ -0,0 +1,28 @@
+package org.yapp.covey.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import org.yapp.covey.R;
+import org.yapp.covey.databinding.LayoutDialogFilterBinding;
+
+public class FilterDialogFragment extends BottomSheetDialogFragment {
+    LayoutDialogFilterBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.layout_post_detail,container,false);
+
+
+        return binding.getRoot();
+    }
+}

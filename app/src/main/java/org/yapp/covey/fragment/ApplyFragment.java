cbd8b492c51b71c5c777f89e0953bee3a0c2d583
GowoonJ <jgw971229@naver.com>
2020-01-09 15:18:22 +0900

    apply, recruit fragment title bar text

--- app/src/main/java/org/yapp/covey/fragment/ApplyFragment.java
+++ app/src/main/java/org/yapp/covey/fragment/ApplyFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.yapp.covey.R;
 import org.yapp.covey.activity.MainActivity;
@@ -26,7 +27,12 @@ public class ApplyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_apply, container, false);
 
-        return inflater.inflate(R.layout.fragment_apply, container, false);
+        TextView titleText = rootView.findViewById(R.id.tv_title);
+        titleText.setText("지원 내역");
+
+
+        return rootView;
     }
 }
--- app/src/main/java/org/yapp/covey/fragment/RecruitFragment.java
+++ app/src/main/java/org/yapp/covey/fragment/RecruitFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.yapp.covey.R;
 
@@ -19,8 +20,13 @@ public class RecruitFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recruit, container, false);
+
+        TextView titleText = rootView.findViewById(R.id.tv_title);
+        titleText.setText("모집 내역");
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recruit, container, false);
+        return rootView;
     }
 
 }

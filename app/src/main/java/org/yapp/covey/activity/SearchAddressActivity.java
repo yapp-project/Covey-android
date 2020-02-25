eed8119a26c800ba6e2624dec3ef0762cae2a37b
GowoonJ <jgw971229@naver.com>
2020-02-26 01:02:44 +0900

    edit Search Address Activity - 서버연결 진행중

--- app/src/main/java/org/yapp/covey/activity/SearchAddressActivity.java
+++ app/src/main/java/org/yapp/covey/activity/SearchAddressActivity.java
@@ -1,21 +1,46 @@
 package org.yapp.covey.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 
 import org.yapp.covey.R;
+import org.yapp.covey.databinding.ActivitySearchAddressBinding;
 import org.yapp.covey.etc.CustomAppBar;
+import org.yapp.covey.model.KaKaoMapSearchModel;
+import org.yapp.covey.util.Singleton;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SearchAddressActivity extends AppCompatActivity {
+
+    private static ActivitySearchAddressBinding binding;
+    private String addressStr;
+    private static final String TAG = "SEARCH_ADDRESS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_address);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_search_address);
 
         setCustomAppBar();
+        binding.editSearchAddress.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int keyCode, KeyEvent keyEvent) {
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    getAddress();
+                    return true;
+                }
+                return false;
+            }
+        });
+        getAddress();
     }
 
     private void setCustomAppBar(){
@@ -28,4 +53,23 @@ public class SearchAddressActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void getAddress(){
+        addressStr = binding.editSearchAddress.getText().toString();
+        String appKey = getResources().getString(R.string.kakao_app_key);
+        Singleton.KakaoMaoApi.categoryList(appKey,1,addressStr).enqueue(new Callback<KaKaoMapSearchModel>() {
+            @Override
+            public void onResponse(Call<KaKaoMapSearchModel> call, Response<KaKaoMapSearchModel> response) {
+                if (response.code() == 200){
+                    Log.d(TAG, "response success");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<KaKaoMapSearchModel> call, Throwable t) {
+                Log.d(TAG, "OnFailure"+ t.toString());
+            }
+        });
+    }
+
 }
